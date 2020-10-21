1bdc31562a3bd8556e98c6fc3c1420c33f9ebd97
brunacmelo <brunacmelo14@gmail.com>
2020-10-21 19:26:49 -0300

    first commit

---
+++ dist/config/keys.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    mongoURI: 'mongodb+srv://bruna:1234@cluster0.w7l1q.mongodb.net/<dbname>?retryWrites=true&w=majority'
+};
+//# sourceMappingURL=keys.js.map
---
+++ src/config/keys.ts
@@ -0,0 +1,3 @@
+export default {
+    mongoURI: 'mongodb+srv://bruna:1234@cluster0.w7l1q.mongodb.net/<dbname>?retryWrites=true&w=majority'
+};
